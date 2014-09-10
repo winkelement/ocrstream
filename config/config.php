4b8e3b6a8eb29c858e82bf6dbaf5af5806d57f25
winkelement <>
2014-09-10 22:05:23 +0200

    clean up

--- config/config.php
+++ config/config.php
@@ -6,7 +6,5 @@
 # which is invoked by choosing Team Centre > Manage Plugins and then clicking on Options for the
 # sample plugin once it has been activated.
 
-$first_words = 'wurst wurst wurst ist alles was ich habe ';      // Indices to the list of pets type giving types owned
 $ocr_global_languages = array();
 $tesseract_path = '/usr/bin';
-//$tesseract_version = string();
