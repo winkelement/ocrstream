c57efbe2cdccbe28adc398e96220cae267015c98
winkelement <>
2014-09-12 15:42:19 +0200

    hook changes

--- hooks/edit.php
+++ hooks/edit.php
@@ -1,12 +1,23 @@
 <?php
-function HookOcrstreamEditEditbeforesectionhead (){
+function HookOcrstreamEditCustompermshowfile (){ // Hook for OCR on single resource (resource edit)
     
     global $ref;
     global $lang;
     ?>
     <div class="Question" id="question_ocr">
     <label for="ocr_single_resource"><?php echo $lang["ocr_single_resource"]?></label>
-    <input type="submit" name="ocr_start" value="<?php echo $lang["ocr_start"]?>" onClick="event.preventDefault();CentralSpacePost(document.getElementById('mainform'),true);">
+    <input type="submit" name="ocr_start" value="<?php echo $lang["ocr_start"]?>" onClick="ocr_single_resource()">
     </div><!-- end of question_copyfrom -->
+    <script>
+    function ocr_single_resource() {
+    document.getElementById("question_ocr").innerHTML = "Hello World";
+    }
+    </script>
     <?php
+    $resource_path = get_resource_path($ref,true,"",false,"");
+    echo $resource_path; // debug
+}
+
+function HookOcrstreamEditBeforeaccessselector () {
+    echo 'blubbblibubbli';
 }
