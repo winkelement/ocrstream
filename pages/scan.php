bd53056ddffe79898c35d434666ce92bb78bdcce
Robert Damrau <winkelement@hotmail.com>
2014-09-23 15:39:41 +0200

    Update scan.php

--- pages/scan.php
+++ pages/scan.php
@@ -1,6 +1,12 @@
 <?php
 
-$ref_id = filter_input(INPUT_GET, 'ref');
-$param1 = filter_input(INPUT_GET, 'param1');
-echo json_encode("Resource ID:" . ' ' . $ref_id . ' ' . "Parameter:" . ' ' . $param1);
+include_once "../../../include/db.php";
+include_once "../../../include/general.php";
+include_once "../../../include/resource_functions.php";
 
+$ref = filter_input(INPUT_GET, 'ref');
+$param1 = filter_input(INPUT_GET, 'param1');
+
+$test_output = ("Resource ID:" . ' ' . $ref . ' ' . "Parameter:" . ' ' . $param1);
+update_field($ref, 72 , $test_output);
+echo json_encode($test_output);
