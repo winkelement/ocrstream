d5099da2dd0c38ba3201ed6be04fa113ce093fff
winkelement <>
2014-09-24 12:53:50 +0200

    testing the ajax call for scan.php, writing test output into database

--- hooks/edit.php
+++ hooks/edit.php
@@ -4,25 +4,28 @@ function HookOcrstreamEditAfterfileoptions (){ // Hook for OCR on single resourc
     global $ref;
     global $lang;
     global $baseurl;
+    global $ocr_global_language;
+    
+    $param_1 = 'X'; // placeholder for optional processing parameter for tesseract
     
     ?>
     <div class="Question" id="question_ocr">
     <label for="ocr_single_resource"><?php echo $lang["ocr_single_resource"]?></label>
-
     <input type="button" name="ocr_start" value="<?php echo $lang["ocr_start"]?>">
     </div><!-- end of question_copyfrom -->
     <script>
         jQuery('[name="ocr_start"]').click(function(){
-
-            jQuery.get('<?php echo $baseurl ?>/plugins/ocrstream/pages/scan.php', { ref: '<?php echo $ref ?>', param1 : 'X'}, function(data){
-                console.log(JSON.parse(data));
+            jQuery.get('<?php echo $baseurl ?>/plugins/ocrstream/pages/scan.php', { ref: '<?php echo $ref ?>', lang : '<?php echo $ocr_global_language ?>', param_1 : '<?php echo $param_1 ?>'}, function(data){
+                var1 = data;
+                console.log(JSON.parse(var1)); // debug
+                alert (JSON.parse(var1)); // debug 
             });
 
         });
     </script>
     <?php
-    $resource_path = get_resource_path($ref,true,"",false,"");
-    echo $resource_path; // debug
+//    $resource_path = get_resource_path($ref,true,"",false,"");
+//    echo $resource_path; // debug
 }
 
 function HookOcrstreamEditBeforeaccessselector () {
--- pages/scan.php
+++ pages/scan.php
@@ -5,8 +5,9 @@ include_once "../../../include/general.php";
 include_once "../../../include/resource_functions.php";
 
 $ref = filter_input(INPUT_GET, 'ref');
-$param1 = filter_input(INPUT_GET, 'param1');
+$lang = filter_input(INPUT_GET, 'lang');
+$param_1 = filter_input(INPUT_GET, 'param_1');
 
-$test_output = ("Resource ID:" . ' ' . $ref . ' ' . "Parameter:" . ' ' . $param1);
-update_field($ref, 72 , $test_output);
+$test_output = ("Resource ID:".' '.$ref.' '."\nOCR-Language:".' '.$lang.' '."\nParameter:".' '.$param_1);
+update_field($ref, 72 , $test_output); // write test_output text (string) to database (metadata field 72)
 echo json_encode($test_output);
