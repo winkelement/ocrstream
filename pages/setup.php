65893562937700735b939cd8dd702720bd9c8f88
winkelement <>
2014-09-24 14:27:51 +0200

    code style

--- pages/setup.php
+++ pages/setup.php
@@ -15,24 +15,26 @@ $page_heading = $lang['ocrstream_title'];
 $page_intro = '<p>' . $lang['ocrstream_intro'] . '</p>';
 $page_def = array();
 
-if (is_tesseract_installed()) {
-    $tesseract_version = get_tesseract_version();
-    $leptonica_version = get_leptonica_version();
-    $tesseract_languages = get_tesseract_languages();
-    //$tess_content = ocr_test('/home/robert/web/eurotext.tif');
-    $page_def[] = config_add_text_input('tesseract_path', $lang['tesseract_path_info']);
-    $page_def[] = config_add_html("<p style=font-size:14px;>$tesseract_version<br>$leptonica_version</p>");
-    if (tesseract_version_is_old())
-    {
-        $page_def[] = config_add_html($lang['tesseract_old_version_info']);
-        $page_def[] = config_add_html("<p<br></p>");
-    }
-    $page_def[] = config_add_single_select('ocr_global_language', $lang['ocrstream_language_select'], $tesseract_languages, $usekeys = false);
+if (is_tesseract_installed()) 
+        {
+        $tesseract_version = get_tesseract_version();
+        $leptonica_version = get_leptonica_version();
+        $tesseract_languages = get_tesseract_languages();
+        //$tess_content = ocr_test('/home/robert/web/eurotext.tif');
+        $page_def[] = config_add_text_input('tesseract_path', $lang['tesseract_path_info']);
+        $page_def[] = config_add_html("<p style=font-size:14px;>$tesseract_version<br>$leptonica_version</p>");
+        if (tesseract_version_is_old())
+                {
+                $page_def[] = config_add_html($lang['tesseract_old_version_info']);
+                $page_def[] = config_add_html("<p<br></p>");
+                }
+        $page_def[] = config_add_single_select('ocr_global_language', $lang['ocrstream_language_select'], $tesseract_languages, $usekeys = false);
     //$page_def[] = config_add_html("<p style=font-size:14px;><br>$tess_content</p>");
-}
-else {
-    $page_def[] = config_add_text_input('tesseract_path', $lang['tesseract_path_input']);
-}
+        }
+else 
+        {
+        $page_def[] = config_add_text_input('tesseract_path', $lang['tesseract_path_input']);
+        }
 
 
 // Build the $page_def array of descriptions of each configuration variable the plugin uses.
