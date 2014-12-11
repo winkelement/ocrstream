8ed1c12e76d3d8bce55dd3e20f4c1173ff31f828
winkelement <winkelement@hotmail.com>
2014-12-11 11:39:59 +0100

    fix os detection

--- include/ocrstream_functions.php
+++ include/ocrstream_functions.php
@@ -8,15 +8,14 @@ function is_windows() {
     $os = php_uname('s');
     if (stristr($os, 'win')) {
         $is_windows = true;
-    }
-    if (stristr($os, 'Darwin')) {
-        $is_windows = false; 
-    } 
-    else {
+    } elseif (stristr($os, 'Darwin')) {
+        $is_windows = false;
+    } else {
         $is_windows = false;
     }
     return $is_windows;
 }
+
 /**
  * Get path to tesseract executable
  * 
