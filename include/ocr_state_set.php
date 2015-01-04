93050784fec4949b2c7945cc389307adda707abd
arotter <andreas.rotter.kontakt@gmail.com>
2015-01-04 16:54:31 +0100

    check for non-false values returned from filter_input

--- include/ocr_state_set.php
+++ include/ocr_state_set.php
@@ -5,14 +5,6 @@ require_once "../../../include/general.php";
 $ref = filter_input(INPUT_GET, 'ref', FILTER_VALIDATE_INT);
 $ocr_state = filter_input(INPUT_GET, 'ocr_state', FILTER_VALIDATE_INT);
 
-sql_query("UPDATE resource SET ocr_state =  '$ocr_state' WHERE ref = '$ref'");
-
-$new_state = sql_value("SELECT ocr_state value FROM resource WHERE ref = '$ref'", '');
-
-echo ($new_state);
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+if ($ref && $ocr_state) {
+    sql_query("UPDATE resource SET ocr_state =  '$ocr_state' WHERE ref = '$ref'");
+}
