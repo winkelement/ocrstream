209ed0db014be53792c08f974da03e2d51e6e648
winkelement <winkelement@hotmail.com>
2015-02-09 16:46:16 +0100

    added config for image processing extension

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 /** Configuration file for ResourceSpace plugin "ocrstream"
  * 
- * These are the default values. They can be overridden by using /plugins/sample/pages/setup.php
+ * These are the default values. They can be overridden by using /plugins/ocrstream/pages/setup.php
  * which is invoked by choosing Team Centre > Manage Plugins and then clicking on Options for the
  * ocrstream plugin once it has been activated.
  */
@@ -14,17 +14,24 @@ $ocr_min_density = '300';
 $ocr_max_density = '600';
 $ocr_min_geometry = '1000';
 $ocr_max_geometry = '2500';
-$ocr_psm_global = 3;
 $ocr_ftype_1 = 72;
 $use_ocr_db_filter = false;
-$ocr_db_filter_1 = "/[^a-zA-Züäöß\\s]/miu";
-$ocr_db_filter_2 = "/(^|\\s)[a-zÜüäÄöÖß0-9](\\s|$)/miu";
 $im_preset_1_density = '300';
 $im_preset_1_geometry = '1024';
 $im_preset_1_quality = '90';
 $im_preset_1_deskew = '40';
 $im_preset_1_sharpen_r = '0';
 $im_preset_1_sharpen_s = '1';
+
+/**
+ * Additional configuration
+ * 
+ * These default values can only be changed here and not via the ocrstream options page
+ */
+$ocr_im_ext = 'jpg';
+$ocr_db_filter_1 = "/[^a-zA-Züäöß\\s]/miu";
+$ocr_db_filter_2 = "/(^|\\s)[a-zÜüäÄöÖß0-9](\\s|$)/miu";
+$ocr_psm_global = 3;
 $ocr_psm_array = array(
 //    0 => 'Orientation and script detection (OSD) only.',
     1 => '1 - Automatic page segmentation with OSD.',
