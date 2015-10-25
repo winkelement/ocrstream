182842eb653c911915c28e7111e51cfa5b2a8431
winkelement <winkelement@hotmail.com>
2015-10-25 13:34:57 +0100

    changed hook for OCR options on edit page

--- hooks/edit.php
+++ hooks/edit.php
@@ -10,7 +10,7 @@ function HookOcrstreamEditEditbeforeheader() {
     }  
 }
 
-function HookOcrstreamEditAfterfileoptions() {
+function HookOcrstreamEditBeforeimagecorrection() {
     global $ref;
     global $lang;
     global $baseurl;
